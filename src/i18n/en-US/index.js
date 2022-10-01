9b1416e5966fd6c83218bdbc96fcb48fd289c3ef
Emanuel Augusto <emanuel@tisaude.com>
2022-10-01 17:04:23 -0300

    adjust

--- src/i18n/en-US/index.js
+++ src/i18n/en-US/index.js
@@ -2,6 +2,8 @@
 // so you can safely delete all default props below
 
 export default {
+  earthDescription: "Earth – our home and the only astronomical object known to harbor life. About 71% of Earth's surface is made up of the ocean, dwarfing Earth's polar ice, lakes, and rivers. The remaining 29% of Earth's surface is land, consisting of continents and islands.",
+  issDescription: "Definition: The International Space Station (ISS) is a space station that keeps moving in low earth orbit. It is habitable and the largest artificial satellite that can be seen with the naked eye from Earth. It acts as a factory, observatory and laboratory.",
   failed: 'Action failed',
   success: 'Action was successful'
 }
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
     <q-scroll-area style="height: 100vh">
-      <div class="row no-wrap">
+      <div
+        class="row no-wrap"
+        style="display: flex; align-items: center; justify-content: center"
+      >
         <q-img
           v-for="(pl, key) in planets"
           :key="key"
           :alt="pl.name"
-          height="600px"
-          width="600px"
+          :height="`${pl.height}px`"
+          :width="`${pl.width}px`"
           :ratio="16 / 9"
           :src="pl.image"
           class="q-ma-md cursor-pointer"
@@ -24,14 +27,14 @@
       v-model="plenetSelected.modal"
       persistent
       maximized
-      transition-show="slide-up"
-      transition-hide="slide-down"
+      transition-show="animated fadeIn"
+      transition-hide="animated fadeIn"
     >
       <q-card v-if="plenetSelected.modal" class="bg-black text-white">
         <q-bar>
           <q-space />
-          <q-btn dense flat icon="close" v-close-popup>
-            <q-tooltip class="bg-white text-primary">Close</q-tooltip>
+          <q-btn dense flat icon="close" color="red" v-close-popup>
+            <q-tooltip class="bg-white text-red">Close</q-tooltip>
           </q-btn>
         </q-bar>
 
@@ -49,6 +52,7 @@
               :initial-ratio="content.ratio"
               class="cursor-pointer"
               :fit="'contain'"
+              @click="navigateCorpusOfPlanet(content)"
             >
             </q-img>
           </div>
@@ -56,6 +60,37 @@
 
         <q-card-section class="q-pt-none">
           {{ plenetSelected.data.description }}
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+
+    <q-dialog
+      v-model="corpusOfPlanet.modal"
+      persistent
+      maximized
+      transition-show="slide-up"
+      transition-hide="slide-down"
+    >
+      <q-card v-if="corpusOfPlanet.modal" class="bg-black text-white">
+        <q-bar>
+          <q-space />
+          <q-btn dense flat icon="close" color="red" v-close-popup>
+            <q-tooltip class="bg-white text-red">Close</q-tooltip>
+          </q-btn>
+        </q-bar>
+
+        <q-card-section>
+          <div class="text-h4 text-bold">{{ corpusOfPlanet.data.name }}</div>
+        </q-card-section>
+
+        <q-card-section
+          v-if="corpusOfPlanet.data.threeDmodelCorpus"
+          v-html="corpusOfPlanet.data.threeDmodelCorpus.url"
+        >
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          {{ $t(corpusOfPlanet.data.description) }}
         </q-card-section>
       </q-card>
     </q-dialog>
@@ -75,8 +110,20 @@ export default defineComponent({
       data: null,
     });
 
+    const corpusOfPlanet = ref({
+      modal: false,
+      data: null,
+    });
+
     function navigatePlanet(pl) {
       plenetSelected.value = {
+        modal: true,
+        data: pl,
+      };
+    }
+
+    function navigateCorpusOfPlanet(pl) {
+      corpusOfPlanet.value = {
         modal: true,
         data: pl,
       };
@@ -87,7 +134,9 @@ export default defineComponent({
     return {
       planets,
       plenetSelected,
+      corpusOfPlanet,
       navigatePlanet,
+      navigateCorpusOfPlanet,
     };
   },
 });
